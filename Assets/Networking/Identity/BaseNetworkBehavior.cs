1d3b20b7bc251b4eadb9a735b554863627c00c82
jjjlalonde@gmail.com <jjjlalonde@gmail.com>
2020-02-01 12:21:59 -0500

    Misc changes

--- Assets/Networking/Identity/BaseNetworkBehavior.cs
+++ Assets/Networking/Identity/BaseNetworkBehavior.cs
@@ -10,6 +10,10 @@ namespace Assets.Networking.Identity
     [RequireComponent(typeof(NetworkIdentity))]
     public abstract class BaseNetworkBehavior : MonoBehaviour, INetworkEntity
     {
+        void Awake()
+        {
+            IdentityStore.NetworkedGameObjectsByGuid.Add(this.AssociatedNetworkIdentity, this.gameObject);
+        }
         public Guid AssociatedNetworkIdentity { get; set; }
         public bool PlayerOwned { get; set; }
     }
