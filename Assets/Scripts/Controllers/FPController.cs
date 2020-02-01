1e2d55321a56b4a6e1b654570e299f733a1aaa20
Cameron Rasico <rasicoca@gmail.com>
2020-02-01 13:10:47 -0500

    finish jumping and gravity

--- Assets/Scripts/Controllers/FPController.cs
+++ Assets/Scripts/Controllers/FPController.cs
@@ -1,23 +1,36 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 namespace Assets.Scripts.Controllers
 {
     public class FPController : MonoBehaviour
     {
-        private float _speed = 10f;
+        public float Speed = 10.0f;
+        public float JumpForce = 50.0f;
+        private Rigidbody _rigidbody;
+        private float _distanceToGround;
 
         void Start()
         {
             Cursor.lockState = CursorLockMode.Locked;
+            _rigidbody = GetComponent<Rigidbody>();
+            _distanceToGround = GetComponent<Collider>().bounds.extents.y;
         }
 
         void Update()
         {
-            var forwardTranslation = Input.GetAxis("Vertical") * _speed;
-            var strafeTranslation = Input.GetAxis("Horizontal") * _speed;
+            var forwardTranslation = Input.GetAxis("Vertical") * Speed;
+            var strafeTranslation = Input.GetAxis("Horizontal") * Speed;
         
             Move(forwardTranslation, strafeTranslation);
+            
+
+            if (IsGrounded() && Input.GetButton("Jump"))
+            {
+                Jump();
+            }
 
             if (Input.GetKey("escape"))
             {
@@ -27,9 +40,8 @@ namespace Assets.Scripts.Controllers
 
         private void OnCollisionExit(Collision other)
         {
-            var rigidBody = GetComponent<Rigidbody>();
-            rigidBody.velocity = Vector3.zero;
-            rigidBody.angularVelocity = Vector3.zero;
+            _rigidbody.velocity = new Vector3(0f, _rigidbody.velocity.y, 0f);
+            _rigidbody.angularVelocity = Vector3.zero;
         }
 
         private void Move(float forwardTranslation, float straffeTranslation)
@@ -38,6 +50,16 @@ namespace Assets.Scripts.Controllers
             straffeTranslation *= Time.deltaTime;
         
             transform.Translate(straffeTranslation, 0, forwardTranslation);
+        }
+
+        private void Jump()
+        {
+            _rigidbody.AddForce(Vector3.up * JumpForce);
+        }
+
+        private bool IsGrounded()
+        {
+            return Physics.Raycast(transform.position, -Vector3.up, _distanceToGround + 0.1f);
         }
 
         private void UnlockCursor()
