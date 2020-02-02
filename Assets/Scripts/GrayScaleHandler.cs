dfee09179b5c985f00ce175fc8dff5f9f31595e9
jjjlalonde@gmail.com <jjjlalonde@gmail.com>
2020-02-01 20:22:30 -0500

    add gray scaling when losing oxygen

---
+++ Assets/Scripts/GrayScaleHandler.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GrayScaleHandler : MonoBehaviour
+{
+    public float scale = 1.0f;
+    public OxygenHandler OxygenHandler;
+
+    private Image grayScaleImage;
+    // Start is called before the first frame update
+    void Start()
+    {
+        OxygenHandler = GameObject.FindObjectOfType<OxygenHandler>();
+        grayScaleImage = GameObject.Find("GrayScalePanel").GetComponent<Image>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        scale =  1.0f - (OxygenHandler.OxygenLeft / 100.0f);
+        var currentColor = grayScaleImage.color;
+        currentColor.a = scale;
+        currentColor.r = currentColor.r * scale;
+        currentColor.g = currentColor.g * scale;
+        currentColor.b = currentColor.b * scale;
+        grayScaleImage.color = currentColor;
+    }
+}
