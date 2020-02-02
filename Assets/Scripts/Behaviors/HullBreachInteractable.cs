ad61adae2de57c5f92723c6514f376104ddd864d
jjjlalonde@gmail.com <jjjlalonde@gmail.com>
2020-02-01 19:52:53 -0500

    Add not even oxygen

--- Assets/Scripts/Behaviors/HullBreachInteractable.cs
+++ Assets/Scripts/Behaviors/HullBreachInteractable.cs
@@ -4,6 +4,19 @@ namespace Assets.Scripts.Behaviors
 {
     public class HullBreachInteractable : MonoBehaviour
     {
+        public void Start()
+        {
+            FindObjectOfType<OxygenHandler>().HullBreaches++;
+            Debug.Log("Adding to hull breaches counter");
+        }
+
+        public void OnDestroy()
+        {
+            FindObjectOfType<OxygenHandler>().HullBreaches--;
+
+            Debug.Log("Decrementing to hull breaches counter");
+        }
+
         public void Interact()
         {
             Destroy(this.gameObject);
---
+++ Assets/Scripts/OxygenBar.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class OxygenBar : MonoBehaviour
+    {
+        public Transform Bar;
+        public OxygenHandler OxygenHandler;
+
+
+        public void Start()
+        {
+            OxygenHandler = GameObject.FindObjectOfType<OxygenHandler>();
+        }
+
+        public void Update()
+        {
+            Bar.localScale = new Vector3(Math.Max(0, (float)OxygenHandler.OxygenLeft / OxygenHandler.Max), 1f);
+        }
+    }
+}
